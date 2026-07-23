f7b0b697fb034f9aca74a3adfd5296ff3ac4e9e4
kqapec <kqapec2@gmail.com>
2026-07-23 22:16:07 +0200

    Update index.js

--- 6/1-node-farm/index.js
+++ 6/1-node-farm/index.js
@@ -39,14 +39,22 @@ const url = require('url');
 /////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+const productObj = JSON.parse(data);
+
+
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview'){
         res.end('This is the OVERVIEW');
-    } else if (pathName === '/product'){
+    }else if (pathName === '/product') {
         res.end('This is the PRODUCT');
-    }else{
+    }else if (pathName === '/api') {
+            res.writeHead(200, {'content-type': 'application/json'});
+            res.end(data);
+    } else {
         res.writeHead(404,{
             'Content-type': 'text/html'
         });
@@ -55,7 +63,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () =>{
-    console.log('Listening to request on port 8000');
-
+   console.log('Listening to request on port 8000');
 });
 
